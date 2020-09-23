6081a4507caa91bbd5ccb09a0b6a21e24ddbf20e
DenTaku <dentaku1412@gmail.com>
2020-09-23 20:12:10 +0900

    Add "index.html"

--- club_tasks/src/main.rs
+++ club_tasks/src/main.rs
@@ -1,15 +1,32 @@
 use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
+use askama::Template;
 use thiserror::Error;
 
+struct Null {}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemlate {
+    entries: Vec<Null>,
+}
+
 #[derive(Error, Debug)]
-enum MyError {}
+enum MyError {
+    #[error("Failed to render HTML")]
+    AskamaError(#[from] askama::Error),
+}
 
 impl ResponseError for MyError {}
 
 #[get("/")]
-async fn index() -> Result<HttpResponse, actix_web::Error> {
-    let response_body = "Hello world!";
-    Ok(HttpResponse::Ok().body(response_body))
+async fn index() -> Result<HttpResponse, MyError> {
+    let mut entries = Vec::new();
+    entries.push(Null{});
+    let html = IndexTemlate { entries };
+    let response_body = html.render()?;
+    Ok(HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response_body))
 }
 
 #[actix_rt::main]
