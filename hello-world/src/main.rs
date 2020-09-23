6bc2bd9d1e2aa786e403ea5184f405cd91eb33b6
DenTaku <dentaku1412@gmail.com>
2020-09-23 23:43:28 +0900

    Learn application scope and State

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -14,14 +14,32 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+// async fn index() -> impl Responder {
+//     "Hello world!"
+// }
+
+struct AppState {
+    app_name: String,
+}
+
+#[get("/")]
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name;
+    format!("Hello {}!", app_name)
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            // .service(hello)
+            // .service(echo)
+            // .route("/hey", web::get().to(manual_hello))
+            // .service(web::scope("/app").route("/index.html", web::get().to(index)))
+            .data(AppState {
+                app_name: String::from("Actix-web"),
+            })
+            .service(index)
     })
     .bind("127.0.0.1:8080")?
     .run()
